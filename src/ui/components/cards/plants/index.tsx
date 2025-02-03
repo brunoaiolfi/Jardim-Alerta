4ea190f0c5e290759235619c8c52713a3bed1f1a
bruno aiolfi <bruno.aiolfi154@gmail.com>
2025-02-03 17:06:09 -0300

    adicionando as imagens aos cards

--- src/ui/components/cards/plants/index.tsx
+++ src/ui/components/cards/plants/index.tsx
@@ -4,13 +4,30 @@ import * as Styles from "./styles"
 
 export function CardPlant({ plant }: ICardPlants) {
 
-    // const Image = require(`../../../../assets/svgs/${plant.name}.svg`);
+    const images: Record<string, any> = {
+        "Aningapara": require('../../../../assets/imgs/Aningapara.jpg'),
+        "Espada São Jorge": require('../../../../assets/imgs/EspadaSaoJorge.jpg'),
+        "Frutíferas": require('../../../../assets/imgs/Frutíferas.jpg'),
+        "Hortênsia": require('../../../../assets/imgs/Hortênsia.jpg'),
+        "Imbé": require('../../../../assets/imgs/Imbé.jpg'),
+        "Orquídea": require('../../../../assets/imgs/Orquídea.jpg'),
+        "Peperomia": require('../../../../assets/imgs/Peperomia.jpg'),
+        "Violeta": require('../../../../assets/imgs/Violeta.jpg'),
+        "Yucca": require('../../../../assets/imgs/Yucca.jpg'),
+        "Zamioculca": require('../../../../assets/imgs/Zamioculca.jpg'),
+    }
+
     return (
         <Styles.Container>
-            {/* <Image /> */}
-            <TextComponent
-                text={plant.name}
+            <Styles.PlantImage
+                source={images[plant.name]}
             />
+            <Styles.Footer>
+                <TextComponent
+                    text={plant.name}
+                    color="white"
+                />
+            </Styles.Footer>
         </Styles.Container>
     )
 }
--- src/ui/components/cards/plants/styles.ts
+++ src/ui/components/cards/plants/styles.ts
@@ -7,6 +7,31 @@ export const Container = styled.View`
     background: ${({ theme }) => theme.colors.transparent};
     border-radius: 20px;
     
-    padding: 20px;
     margin: 10px;
+
+    justify-content: flex-end;
+`;
+
+export const PlantImage = styled.Image`
+    width: 100%;
+    height: 100%;
+    
+    border-radius: 20px;
+    resize-mode: cover;
+
+    position: absolute;
+`;
+
+export const Footer = styled.View`
+    width: 100%;
+
+    padding: 8px 0 ;
+
+    background: rgba(0, 0, 0, 0.7);
+
+    justify-content: center;
+    align-items: center;
+
+    border-bottom-left-radius: 20px;
+    border-bottom-right-radius: 20px;
 `;
