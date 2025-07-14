3b5b101cc1b064eed75f625ce8613b88f4b59209
bruno aiolfi <bruno.aiolfi154@gmail.com>
2025-07-14 17:01:28 -0300

    Fix/ corrigindo as depêndencias do tanstack

--- babel.config.js
+++ babel.config.js
@@ -4,6 +4,9 @@ module.exports = {
     '@babel/plugin-proposal-export-namespace-from',
     'react-native-reanimated/plugin',
     'babel-plugin-transform-typescript-metadata',
-    ['@babel/plugin-proposal-decorators', {legacy: true}],
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-transform-class-properties', { loose: false }],
+    ['@babel/plugin-transform-private-methods', { loose: false }],
+    ['@babel/plugin-transform-private-property-in-object', { loose: false }],
   ]
 };
