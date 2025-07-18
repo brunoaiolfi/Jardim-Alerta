2240b7843dee3b1d05d6291582574deb629a4f7e
bruno aiolfi <bruno.aiolfi154@gmail.com>
2025-07-18 18:04:12 -0300

    fix/corrigindo as dependencias do projeto

--- babel.config.js
+++ babel.config.js
@@ -2,11 +2,13 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     '@babel/plugin-proposal-export-namespace-from',
-    'react-native-reanimated/plugin',
     'babel-plugin-transform-typescript-metadata',
+    '@babel/plugin-transform-flow-strip-types',
     ['@babel/plugin-proposal-decorators', { legacy: true }],
-    ['@babel/plugin-transform-class-properties', { loose: false }],
-    ['@babel/plugin-transform-private-methods', { loose: false }],
-    ['@babel/plugin-transform-private-property-in-object', { loose: false }],
-  ]
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ['@babel/plugin-transform-class-properties', { loose: true }],
+    ['@babel/plugin-transform-private-methods', { loose: true }],
+    ['@babel/plugin-transform-private-property-in-object', { loose: true }],
+    'react-native-reanimated/plugin',
+  ],
 };
